d9acdcf635820a6f2d59a421c9c990887446b072
sai-sourav <saisourav123@gmail.com>
2022-11-15 10:12:52 +0530

    added crud crud feature

--- index.js
+++ index.js
@@ -1,27 +1,48 @@
 const myform = document.getElementById("myform");
 const itemlist = document.getElementById("items");
+const URL = "https://crudcrud.com/api/e7337237d4524c6db9f7b431b5433d5d/Expenses"
 
 myform.addEventListener("submit",onsubmit);
 itemlist.addEventListener("click",deleteitem);
 itemlist.addEventListener("click",edititem);
+window.addEventListener("DOMContentLoaded",showlist);
 
 function onsubmit(e){
     e.preventDefault();
     const amount = document.getElementById("amount").value;
     const desc = document.getElementById("desc").value;
     const category = document.getElementById("category").value;
-
-    additem(amount,desc,category);
-    document.getElementById("amount").value = '';
-    document.getElementById("desc").value = '';
-    document.getElementById("category").value = '';
+    axios.post(`${URL}/`,{
+        "amount" : amount,
+        "desc" : desc,
+        "category" : category
+    }).then(resp => {
+        console.log(resp);
+        showlist();
+        document.getElementById("amount").value = '';
+        document.getElementById("desc").value = '';
+        document.getElementById("category").value = '';
+    })
+    // additem(amount,desc,category);
 }
 
-function additem(amount,desc,category){
+function showlist(){
+    itemlist.innerHTML = "";
+    axios.get(URL)
+    .then(resp => {
+        for(let i=0; i < resp.data.length; i++){
+           additem(resp.data[i])
+        }
+    })
+    .catch(err => console.log(err));
+}
+
+function additem(obj){
 
     let li = document.createElement('li');
-    let text = `amount=${amount} description=${desc} category=${category}`
+    let text = `amount=${obj.amount} description=${obj.desc} category=${obj.category}`
     li.appendChild(document.createTextNode(text));
+    li.id = `${obj._id}`
 
     // add edit button
     let editbtn = document.createElement('button');
@@ -43,7 +64,11 @@ function additem(amount,desc,category){
 function deleteitem(e){
     if (e.target.classList.contains('delete')){
         let li = e.target.parentElement;
-        itemlist.removeChild(li);
+        axios.delete(`${URL}/${li.id}`)
+        .then(resp => {
+            console.log(resp);
+            itemlist.removeChild(li);
+        })
     }
 }
 
@@ -62,6 +87,10 @@ function edititem(e){
         document.getElementById("desc").value = desc;
         document.getElementById("category").value = category;
         let li = e.target.parentElement;
-        itemlist.removeChild(li);
+        axios.delete(`${URL}/${li.id}`)
+        .then(resp => {
+            console.log(resp);
+            itemlist.removeChild(li);
+        })
     }
 }
