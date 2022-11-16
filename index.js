7f89c28b5066b612a305efdb3d58f4520b38b91c
sai-sourav <saisourav123@gmail.com>
2022-11-16 17:48:18 +0530

    modified using try,catch and async await

--- index.js
+++ index.js
@@ -1,40 +1,55 @@
 const myform = document.getElementById("myform");
 const itemlist = document.getElementById("items");
-const URL = "https://crudcrud.com/api/e7337237d4524c6db9f7b431b5433d5d/Expenses"
+const URL = "https://crudcrud.com/api/a62e76d389534934b821c7d7dbcb21cd/Expenses"
 
 myform.addEventListener("submit",onsubmit);
 itemlist.addEventListener("click",deleteitem);
 itemlist.addEventListener("click",edititem);
 window.addEventListener("DOMContentLoaded",showlist);
 
-function onsubmit(e){
+async function onsubmit(e){
     e.preventDefault();
     const amount = document.getElementById("amount").value;
     const desc = document.getElementById("desc").value;
     const category = document.getElementById("category").value;
-    axios.post(`${URL}/`,{
-        "amount" : amount,
-        "desc" : desc,
-        "category" : category
-    }).then(resp => {
-        console.log(resp);
-        showlist();
-        document.getElementById("amount").value = '';
-        document.getElementById("desc").value = '';
-        document.getElementById("category").value = '';
-    })
+    document.getElementById("amount").value = '';
+    document.getElementById("desc").value = '';
+    document.getElementById("category").value = '';
+    // axios.post(`${URL}/`,{
+    //     "amount" : amount,
+    //     "desc" : desc,
+    //     "category" : category
+    // }).then(resp => {
+    //     console.log(resp);
+    //     showlist();
+    // })
     // additem(amount,desc,category);
+    try{
+        let result = await axios.post(`${URL}/`,{ amount, desc, category });
+        let show = await new Promise.resolve(showlist());
+    }
+    catch(e){console.log(e)};
+    
 }
 
-function showlist(){
+async function showlist(){
     itemlist.innerHTML = "";
-    axios.get(URL)
-    .then(resp => {
+    // axios.get(URL)
+    // .then(resp => {
+    //     for(let i=0; i < resp.data.length; i++){
+    //        additem(resp.data[i])
+    //     }
+    // })
+    // .catch(err => console.log(err));
+    try{
+        let resp = await axios.get(URL);
+
         for(let i=0; i < resp.data.length; i++){
-           additem(resp.data[i])
+            additem(resp.data[i])
         }
-    })
-    .catch(err => console.log(err));
+    }catch(e){
+        console.log(e);
+    }
 }
 
 function additem(obj){
@@ -61,18 +76,25 @@ function additem(obj){
 
 }
 
-function deleteitem(e){
+async function deleteitem(e){
     if (e.target.classList.contains('delete')){
         let li = e.target.parentElement;
-        axios.delete(`${URL}/${li.id}`)
-        .then(resp => {
-            console.log(resp);
+        // axios.delete(`${URL}/${li.id}`)
+        // .then(resp => {
+        //     console.log(resp);
+        //     itemlist.removeChild(li);
+        // })
+        try{
+            let resp = await axios.delete(`${URL}/${li.id}`);
             itemlist.removeChild(li);
-        })
+        }
+        catch(e){
+            console.log(e);
+        }
     }
 }
 
-function edititem(e){
+async function edititem(e){
     if (e.target.classList.contains('edit')){
         let text = e.target.parentElement.innerText;
         text = text.replace('EDITX','');
@@ -86,11 +108,18 @@ function edititem(e){
         document.getElementById("amount").value = amount;
         document.getElementById("desc").value = desc;
         document.getElementById("category").value = category;
+        // deleting element
         let li = e.target.parentElement;
-        axios.delete(`${URL}/${li.id}`)
-        .then(resp => {
-            console.log(resp);
+        // axios.delete(`${URL}/${li.id}`)
+        // .then(resp => {
+        //     console.log(resp);
+        //     itemlist.removeChild(li);
+        // })
+        try{
+            let resp = await axios.delete(`${URL}/${li.id}`);
             itemlist.removeChild(li);
-        })
+        }catch(e){
+            console.log(e);
+        }
     }
 }
